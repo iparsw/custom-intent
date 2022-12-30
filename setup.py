6d95fc233ba3c51f186c69a78fb5e2d952a82bbb
parsa <parsaroshanak@gmail.com>
2022-12-30 15:08:08 +0330

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CustomIntents",
+    version="0.1.0",
+    author="Parsa Roshanak (iparsw) ",
+    author_email="parsaroshanak@gmail.com",
+    long_description=long_description,
+    long_descriptio_content_type="text/markdown",
+    url="",
+    packages=["CustomIntents"],
+    install_requires=[]
+)
