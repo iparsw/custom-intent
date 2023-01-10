34940f3fed5a57bf5334dbe19f16b5771ad77ecd
parsa <parsaroshanak@gmail.com>
2023-01-10 22:27:35 +0330

    0.7.0

--- CustomIntents/__init__.py
+++ CustomIntents/__init__.py
@@ -1,6 +1,4 @@
-from CustomIntents.customIntents import JsonIntents, PLinearRegression, BinaryImageClassificate
 from . import Pfunctions
 from Chatbot import ChatBot
 from JsonIntents import JsonIntents
 from PLinearRegression import PLinearRegression
-
